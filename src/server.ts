ac2d30ac982ef0a3b17eb930258f22afdf499caa
Natália Fonseca <nataliabf@outlook.com>
2022-08-30 03:13:48 -0300

    chore: add helmet and winston

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,28 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
-// import swaggerUi from "swagger-ui-express";
+import helmet from "helmet";
 import morgan from "morgan";
+
+import { logger } from "./utils/logger";
 
 dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: "*" }));
-app.use(express.json());
-
-app.use(morgan("dev"));
-
-// app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
-app.use(express.urlencoded({ extended: true }));
-app.get("/", (req, res) => res.json({ message: "Welcome to Woofs API" }));
-
 const PORT = process.env.PORT || 3333;
 
-app.listen(PORT, () => console.log(`✅ Server running on http://localhost:${PORT}`));
+app.use(express.json());
+app.use(cors({ origin: "*" }));
+app.use(helmet());
+app.use(
+  morgan(":method :url :status :response-time ms - :res[content-length]", {
+    stream: {
+      write: (message) => logger.http(message.trim()),
+    },
+  }),
+);
+
+app.get("/", (req, res) => res.json({ message: "Welcome to Woofs API" }));
+
+app.listen(PORT, () => logger.info(`✅ Server running on http://localhost:${PORT}`));
